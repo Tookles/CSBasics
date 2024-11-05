0249fbd30ac2e8b21a1012afe4afd0adbd557608
Tookey <rachel.tookey@gmail.com>
2024-11-05 15:59:37 +0000

    Work until exercise 9

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -4,7 +4,48 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            double pi = 3.14;
+
+            bool isLearningCSharp = true;
+
+            string favFilm = "Lion King";
+
+            favFilm = "High School Musical";
+
+            int number = new int();
+
+            string name = "Persephone"; 
+
+            printGreeting(name);
+
+            numberSubtract(10, 5);
+
+            Console.WriteLine(isStringLonger("banana", 14));
+
+            int additionResult = Sum(10, 5);
+            Console.WriteLine(additionResult);
+
+        }
+        
+        private static void printGreeting(string name)
+        {
+            Console.WriteLine($"Hello {name}!");
+
+        }
+
+        private static int numberSubtract(int numberOne, int numberTwo)
+        {
+            return numberOne - numberTwo; 
+        }
+
+        private static bool isStringLonger(string word, int number)
+        {
+            return word.Length > number; 
+        }
+
+        private static int Sum(int numberOne, int numberTwo)
+        {
+            return numberOne + numberTwo;
         }
     }
 }
