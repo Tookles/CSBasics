373556649923391d5b9c75b2754e26afed5c6609
PomPomegranates <persephoneapplewhite@gmail.com>
2024-11-05 16:29:24 +0000

    added Book

--- CSBasics/Book.cs
+++ CSBasics/Book.cs
@@ -3,10 +3,35 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Transactions;
 
 namespace CSBasics
 {
     internal class Book
     {
+        public string Title { get; set; }
+        public int Pages { get; set; }
+
+        public int CurrentPage { get; set; } = 1;
+        
+
+        public Book(string title, int pages)
+        {
+            Title = title;
+            Pages = pages;
+        }
+
+        public void TurnPage()
+        {
+            if (CurrentPage == Pages)
+            {
+                Console.WriteLine("You've reached the end of the book");
+            }
+            else
+            {
+                CurrentPage++;
+                Console.WriteLine($"You turned the page! Your current page is {CurrentPage}");
+            };
+        }
     }
 }
