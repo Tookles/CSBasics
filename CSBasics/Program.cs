b0b6722b21c2b3a5e2f60f6cb6aa4b401d56690d
PomPomegranates <persephoneapplewhite@gmail.com>
2024-11-05 16:29:59 +0000

    updated the Program file

--- CSBasics/Program.cs
+++ CSBasics/Program.cs
@@ -4,29 +4,36 @@
     {
         static void Main(string[] args)
         {
-            double pi = 3.14;
+            //double pi = 3.14;
 
-            bool isLearningCSharp = true;
+            //bool isLearningCSharp = true;
 
-            string favFilm = "Lion King";
+            //string favFilm = "Lion King";
 
-            favFilm = "High School Musical";
+            //favFilm = "High School Musical";
 
-            int number = new int();
+            //int number = new int();
 
-            string name = "Persephone"; 
+            //string name = "Persephone"; 
 
-            printGreeting(name);
+            //printGreeting(name);
 
-            numberSubtract(10, 5);
+            //numberSubtract(10, 5);
 
-            Console.WriteLine(isStringLonger("banana", 14));
+            ////Console.WriteLine(isStringLonger("banana", 14));
 
-            int additionResult = Sum(10, 5);
-            Console.WriteLine(additionResult);
+            //int additionResult = Sum(10, 5);
+            ////Console.WriteLine(additionResult);
+            Book lionWitchWardrobe = new Book("The Lion, The Witch and the Wardrobe", 100);
+
+            Console.WriteLine($"You are reading {lionWitchWardrobe.Title} and you are on page {lionWitchWardrobe.CurrentPage}");
+            lionWitchWardrobe.TurnPage();
+            lionWitchWardrobe.TurnPage();
+            lionWitchWardrobe.TurnPage();
+            lionWitchWardrobe.TurnPage();
 
         }
-        
+
         private static void printGreeting(string name)
         {
             Console.WriteLine($"Hello {name}!");
